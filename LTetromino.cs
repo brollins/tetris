4c2e6ab9032f5ce223d5e06ea0d0ce573d2341a8
thinkgeo.bradrollins <bradrollins@thinkgeo.com>
2016-10-18 16:28:28 -0500

    final tetromino rotation complete

--- LTetromino.cs
+++ LTetromino.cs
@@ -6,6 +6,11 @@ namespace Tetris
 {
     public class LTetromino : Tetromino
     {
+        private bool isUpright = true;
+        private bool isRight = false;
+        private bool isLeft = false;
+        private bool isDown = false;
+
         public LTetromino(Canvas canvas) : base(canvas)
         {
             this.Blocks.Add(new TetrisBlock(200, 1, this.Color, canvas));
@@ -16,12 +21,116 @@ namespace Tetris
 
         public override void RotateCounterClockwise()
         {
-            throw new NotImplementedException();
+            if (isUpright)
+            #region
+            {
+                Blocks[0].X -= 100;
+                Blocks[0].Y += 100;
+                Blocks[1].X -= 50;
+                Blocks[1].Y += 50;
+                Blocks[3].X -= 50;
+                Blocks[3].Y -= 50;
+                Draw();
+                isUpright = false;
+                isLeft = true;
+            }
+
+            else if (isLeft)
+            {
+                Blocks[0].X += 100;
+                Blocks[0].Y += 100;
+                Blocks[1].X += 50;
+                Blocks[1].Y += 50;
+                Blocks[3].X -= 50;
+                Blocks[3].Y += 50;
+                Draw();
+                isLeft = false;
+                isDown = true;
+            }
+
+            else if (isDown)
+            {
+                Blocks[0].X += 100;
+                Blocks[0].Y -= 100;
+                Blocks[1].X += 50;
+                Blocks[1].Y -= 50;
+                Blocks[3].X += 50;
+                Blocks[3].Y += 50;
+                Draw();
+                isDown = false;
+                isRight = true;
+            }
+
+            else if (isRight)
+            {
+                Blocks[0].X -= 100;
+                Blocks[0].Y -= 100;
+                Blocks[1].X -= 50;
+                Blocks[1].Y -= 50;
+                Blocks[3].X += 50;
+                Blocks[3].Y -= 50;
+                Draw();
+                isRight = false;
+                isUpright = true;
+            }
+            #endregion
         }
 
         public override void RotateClockwise()
         {
-            throw new NotImplementedException();
+            if (isUpright)
+            #region
+            {
+                Blocks[0].X += 100;
+                Blocks[0].Y += 100;
+                Blocks[1].X += 50;
+                Blocks[1].Y += 50;
+                Blocks[3].X -= 50;
+                Blocks[3].Y += 50;
+                Draw();
+                isUpright = false;
+                isRight = true;
+            }
+
+            else if (isLeft)
+            {
+                Blocks[0].X += 100;
+                Blocks[0].Y -= 100;
+                Blocks[1].X += 50;
+                Blocks[1].Y -= 50;
+                Blocks[3].X += 50;
+                Blocks[3].Y += 50;
+                Draw();
+                isLeft = false;
+                isUpright = true;
+            }
+
+            else if (isDown)
+            {
+                Blocks[0].X -= 100;
+                Blocks[0].Y -= 100;
+                Blocks[1].X -= 50;
+                Blocks[1].Y -= 50;
+                Blocks[3].X += 50;
+                Blocks[3].Y -= 50;
+                Draw();
+                isDown = false;
+                isLeft = true;
+            }
+
+            else if (isRight)
+            {
+                Blocks[0].X -= 100;
+                Blocks[0].Y += 100;
+                Blocks[1].X -= 50;
+                Blocks[1].Y += 50;
+                Blocks[3].X -= 50;
+                Blocks[3].Y -= 50;
+                Draw();
+                isRight = false;
+                isDown = true;
+            }
+            #endregion
         }
     }
 }
